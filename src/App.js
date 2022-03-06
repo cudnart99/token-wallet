dbe0f5d8a3728e52043ae1a8d3d33c3d8a464fd1
tuan.dq <tuan.dq@isofhcare.com>
2022-03-06 15:04:37 +0700

    #connect multiple wallet

--- src/App.js
+++ src/App.js
@@ -1,39 +1,196 @@
-import { useState, useEffect } from 'react';
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { providers } from "ethers";
+import Head from "next/head";
+import { useCallback, useEffect, useReducer } from "react";
+import WalletLink from "walletlink";
+import Web3Modal from "web3modal";
 import AppAuthenticated from "./components/AppAuthenticated";
-import Web3 from 'web3/dist/web3.min.js';
 
-const App = () => {
-  const [walletConnected, setWalletConnected] = useState(false);
-  const [instruction, setInstruction] = useState("Waiting for connection with wallet...");
+const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
 
-  useEffect(() => {
-    const connectWallet = async () => {
-      if(!window.ethereum)
-        return;
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: INFURA_ID, // required
+    },
+  },
+  "custom-walletlink": {
+    display: {
+      logo: "https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0",
+      name: "Coinbase",
+      description: "Connect to Coinbase Wallet (not Coinbase App)",
+    },
+    options: {
+      appName: "Coinbase", // Your app name
+      networkUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+      chainId: 1,
+    },
+    package: WalletLink,
+    connector: async (_, options) => {
+      const { appName, networkUrl, chainId } = options;
+      const walletLink = new WalletLink({
+        appName,
+      });
+      const provider = walletLink.makeWeb3Provider(networkUrl, chainId);
+      await provider.enable();
+      return provider;
+    },
+  },
+};
 
-      try {
-        await window.ethereum.send('eth_requestAccounts');
-        window.web3 = new Web3(window.ethereum);
-      } catch (error) {
-        setInstruction("Wallet connection denied, reload the page to try again.");
-        return;
-      }
-      setInstruction("");
-      setWalletConnected(true);
-    };
-    connectWallet();
-  }, []);
-
-  return (
-    <div>
-      {window.ethereum ?
-        (walletConnected ?
-          <AppAuthenticated/>
-          : instruction)
-        : "Metamask or other EIP-1102 / EIP-1193 compliant wallet not found."
-      }
-    </div>
-  )
+let web3Modal;
+if (typeof window !== "undefined") {
+  web3Modal = new Web3Modal({
+    network: "mainnet", // optional
+    cacheProvider: true,
+    providerOptions, // required
+  });
 }
 
-export default App
+const initialState = {
+  provider: null,
+  web3Provider: null,
+  address: null,
+  chainId: null,
+};
+
+function reducer(state = {}, action = {}) {
+  switch (action.type) {
+    case "SET_WEB3_PROVIDER":
+      return {
+        ...state,
+        provider: action.provider,
+        web3Provider: action.web3Provider,
+        address: action.address,
+        chainId: action.chainId,
+      };
+    case "SET_ADDRESS":
+      return {
+        ...state,
+        address: action.address,
+      };
+    case "SET_CHAIN_ID":
+      return {
+        ...state,
+        chainId: action.chainId,
+      };
+    case "RESET_WEB3_PROVIDER":
+      return initialState;
+    default:
+      throw new Error();
+  }
+}
+
+export const Home = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { provider, web3Provider, address, chainId } = state;
+
+  const connect = useCallback(async function () {
+    // This is the initial `provider` that is returned when
+    // using web3Modal to connect. Can be MetaMask or WalletConnect.
+    const provider = await web3Modal.connect();
+
+    // We plug the initial `provider` into ethers.js and get back
+    // a Web3Provider. This will add on methods from ethers.js and
+    // event listeners such as `.on()` will be different.
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const signer = web3Provider.getSigner();
+    const address = await signer.getAddress();
+
+    const network = await web3Provider.getNetwork();
+
+    dispatch({
+      type: "SET_WEB3_PROVIDER",
+      provider,
+      web3Provider,
+      address,
+      chainId: network.chainId,
+    });
+  }, []);
+
+  const disconnect = useCallback(
+    async function () {
+      await web3Modal.clearCachedProvider();
+      if (provider?.disconnect && typeof provider.disconnect === "function") {
+        await provider.disconnect();
+      }
+      dispatch({
+        type: "RESET_WEB3_PROVIDER",
+      });
+    },
+    [provider]
+  );
+
+  // Auto connect to the cached provider
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      connect();
+    }
+  }, [connect]);
+
+  // A `provider` should come with EIP-1193 events. We'll listen for those events
+  // here so that when a user switches accounts or networks, we can update the
+  // local React state with that new information.
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts = []) => {
+        // eslint-disable-next-line no-console
+        console.log("accountsChanged", accounts);
+        dispatch({
+          type: "SET_ADDRESS",
+          address: accounts[0],
+        });
+      };
+
+      // https://docs.ethers.io/v5/concepts/best-practices/#best-practices--network-changes
+      const handleChainChanged = (_hexChainId) => {
+        window.location.reload();
+      };
+
+      const handleDisconnect = ({ code, message }) => {
+        // eslint-disable-next-line no-console
+        console.log("disconnect", code + message);
+        disconnect();
+      };
+
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
+
+      // Subscription Cleanup
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
+        }
+      };
+    }
+  }, [provider, disconnect]);
+
+  // const chainData = getChainData(chainId);
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Web3Modal Example</h1>
+        {web3Provider ? (
+          <AppAuthenticated />
+        ) : (
+          <button className="button" type="button" onClick={connect}>
+            Connect
+          </button>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
